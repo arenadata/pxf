8a99bff871338653628d81a531a3ddc3fb0fcd9f
Alexey Gordeev <goa@arenadata.io>
2023-02-20 17:05:57 +0500

    Dockerfile and test script improvements

--- arenadata/test_in_docker.sh
+++ arenadata/test_in_docker.sh
@@ -3,12 +3,8 @@
 
 # manually prepare gpadmin user; test_pxf.bash doesn't tweak gpadmin folder permissions and ssh keys
 ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash
-# test_pxf.bash use relative paths for unpacking gpdb commands and sourcing of env vars
-cd /tmp/build
-ln -s /home/gpadmin/gpdb_src /tmp/build/gpdb_src 
-ln -s /home/gpadmin/bin_gpdb /tmp/build/bin_gpdb
 # unpack gpdb and pxf; run gpdb cluster and pxf server
-pxf_src/concourse/scripts/test_pxf.bash
+/tmp/build/pxf_src/concourse/scripts/test_pxf.bash
 # tweak necessary folders to run regression tests later
 chown gpadmin:gpadmin -R /usr/local/greenplum-db-devel
 chown gpadmin:gpadmin -R /tmp/build/pxf_src
